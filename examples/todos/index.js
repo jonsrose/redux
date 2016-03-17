100e1c00dafb14be083248908e36e27e4a8a6387
Jon Rose <jon@jonrose.com>
2016-03-17 09:11:30 -0700

    move redux to common

--- examples/todos/index.js
+++ examples/todos/index.js
@@ -4,9 +4,9 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
-import todoApp from './reducers'
-import App from './components/App'
-import { fetchTodos } from './actions'
+import todoApp from './common/reducers'
+import App from './common/components/App'
+import { fetchTodos } from './common/actions'
 
 let store =  applyMiddleware(thunk)(createStore)(todoApp);
 
