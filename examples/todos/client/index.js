f9871250866a8bbaa2300506550ee0cf1710523f
Jon Rose <jon@jonrose.com>
2016-03-17 09:18:37 -0700

    move redux to common

--- examples/todos/client/index.js
+++ examples/todos/client/index.js
@@ -4,11 +4,11 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
-import todoApp from './common/reducers'
-import App from './common/components/App'
-import { fetchTodos } from './common/actions'
+import todoApp from '../common/reducers'
+import App from '../common/components/App'
+import { fetchTodos } from '../common/actions'
 
-let store =  applyMiddleware(thunk)(createStore)(todoApp);
+let store = applyMiddleware(thunk)(createStore)(todoApp);
 
 store.dispatch(fetchTodos())
 
--- examples/todos/webpack.config.js
+++ examples/todos/webpack.config.js
@@ -2,10 +2,10 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'inline-source-map',
   entry: [
     'webpack-hot-middleware/client',
-    './index'
+    './client/index.js'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
