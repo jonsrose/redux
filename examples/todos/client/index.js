fdec136359d948d4c63f65a51895777d81e9b9d7
Jon Rose <jon@jonrose.com>
2016-03-17 15:01:35 -0700

    configureStore

--- examples/todos/client/index.js
+++ examples/todos/client/index.js
@@ -2,13 +2,11 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk';
-import todoApp from '../common/reducers'
 import App from '../common/components/App'
 import { fetchTodos } from '../common/actions'
+import configureStore from '../common/store/configureStore'
 
-let store = applyMiddleware(thunk)(createStore)(todoApp);
+const store = configureStore()
 
 store.dispatch(fetchTodos())
 
---
+++ examples/todos/common/store/configureStore.js
@@ -0,0 +1,24 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from '../reducers'
+
+//export default function configureStore(initialState) {
+export default function configureStore() {
+  const store = createStore(
+    rootReducer,
+    //initialState,
+    applyMiddleware(thunk)
+  )
+
+  /*
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      const nextRootReducer = require('../reducers').default
+      store.replaceReducer(nextRootReducer)
+    })
+  }
+  */
+
+  return store
+}
